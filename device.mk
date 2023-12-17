127893d97156995033349290e0e4dfd2bdd1294e
Kizziama <kizziama@proton.me>
2023-12-17 08:18:41 +0000

    topaz: Build missing NFC packages
    * com.android.nfc_extras
    * NfcNci
    * SecureElement
    
    Change-Id: I475a9b6977bd9f6918144127b363e5d36d30b4d6

--- device.mk
+++ device.mk
@@ -357,7 +357,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service.st \
     android.hardware.secure_element@1.2.vendor \
+    com.android.nfc_extras \
     libchrome.vendor \
+    NfcNci \
+    SecureElement \
     Tag
 
 PRODUCT_COPY_FILES += \
